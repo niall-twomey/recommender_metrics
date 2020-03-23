aa4bf8c364691dab9316176db52a915637a830fe
Niall Twomey <niall-twomey@cookpad.com>
2020-03-23 10:18:53 +0000

    initial commit

---
+++ recommender_metrics/random_data.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+
+
+def generate_random_data(n_users=20, n_items=100, n_interactions_per_user=10, random_state=1234):
+    rng = np.random.RandomState(random_state)
+
+    data = []
+    for user_id in range(n_users):
+        n_user_interactions = rng.randint(1, n_interactions_per_user)
+        for recipe_id in rng.choice(n_items, n_user_interactions):
+            data.append(dict(
+                group_id=user_id,
+                user_id=user_id,
+                item_id=recipe_id,
+                score=rng.normal(),
+                label=int(rng.rand() > 0.75),
+            ))
+
+    return pd.DataFrame(
+        data
+    ).sort_values(
+        by=['group_id', 'score'],
+        ascending=[True, False]
+    )
