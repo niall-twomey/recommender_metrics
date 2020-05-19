4af73875a38bcd0bdf337d518c7962a6c690855b
Niall Twomey <niall-twomey@cookpad.com>
2020-05-19 11:01:52 +0100

    fixed version duplication

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
+from distutils.util import convert_path
+
 import setuptools
 
+# Load the readme
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+# Load the version info
+version_namespace = {}
+ver_path = convert_path("recommender_metrics/version.py")
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), version_namespace)
+
+# Execute the setup
 setuptools.setup(
     name="recommender-metrics",
-    version="0.1.9",
+    version=version_namespace["__version__"],
     author="Niall Twomey",
     author_email="twomeynj@gmail.com",
     description="Recommender metric evaluation",
